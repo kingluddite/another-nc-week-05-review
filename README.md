77cd3e81e316841a3767366e180e216a24937d37
philip howley <howley.phil@gmail.com>
2021-11-27 09:39:10 -0500

    Add function declaration

---
+++ README.md
@@ -0,0 +1,10 @@
+# Review Week 5
+
+![mastering markdown](https://i.imgur.com/CpUeZeJ.png)
+
+- [Mastering Markdown](https://masteringmarkdown.com/)
+
+## Useful Links
+
+- https://www.toptal.com/developers/gitignore
+- https://reactsensei.com/favicon/
---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+// document.getElementById("demo").innerHTML = "Paragraph <strong>changed</strong> again!";
+// // console.log('it works!');
+// const numberOfInches = 100;
+
+// function inchesToCm(inches) {
+//     return inches * 2.54; 
+// }
+
+// const lengthCm = inchesToCm(numberOfInches);
+// console.log(lengthCm);
+
+
+
